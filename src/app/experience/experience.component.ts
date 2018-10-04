60e39281a5a98d9a91542912cd0837d123c6a795
Amelia Huang <huijie.huang@student.manchester.ac.uk>
2018-10-04 11:11:45 +0100

    added experience, smaller photos

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -17,22 +17,22 @@ export class ExperienceComponent implements OnInit {
         "Accenture",
         "Technology Placement",
         "London, Jul 2017 - 2018",
-        "This internship was a great introduction into the consulting industry and allowed me to try out different roles. I managed to pull my own weight while working in teams of experienced professionals, and their trust in me really boosted my confidence in my own code. It was amazing to be working with passionate people who all possess expertise in various fields and to have the opportunity to learn from people with so many different backgrounds. The feedback I received from my supervisors really helped me understand my professional strengths and weaknesses."
+        "This internship was a great introduction into the consulting industry and allowed me to try out different roles. I managed to pull my own weight while working in teams of experienced developers, and their trust in me really boosted my confidence in my own code and capabilities. It was awesome to be working with and learning from passionate people who all possessed expertise in different fields! The feedback I received also really helped me understand my professional strengths and weaknesses."
       ),
       new Experience(
         "assets/img/vhub.png",
         "Volleyball Hub",
         "Digital Marketing Associate",
         "London/Remote, Sep 2017 - present",
-        "I helped market and promote a new business that provides skill clinics for local players. Volleyball is a strong passion of mine and it was amazing to share this passion with others, and to help develop the London volleyball community. Apart from developing my business acumen and people skills, this role also helped stretch my creative abilities and event management capabilities."
-      )
-      // new Experience(
-      //   "assets/img/ttp.png",
-      //   "To The Point Pte Ltd",
-      //   "Work Experience",
-      //   "Singapore, August 2015",
-      //   "das"
-      // ),
+        "I helped market and promote a new business that provides skill clinics for local players. Volleyball is a strong passion of mine and I love to share this passion with others, and to help develop the London volleyball community. Apart from developing my business acumen and people skills, this role also helped stretch my creative and event management capabilities."
+      ),
+      new Experience(
+        "assets/img/ttp.png",
+        "To The Point Pte Ltd",
+        "Work Experience",
+        "Singapore, August 2015",
+        "TTP is a provider of enterprise IT software, and my short stint here was an appetiser of what it was like to work in technology. This was where I first saw how much everyday business can be made more efficient with technology! It was an amazing feeling to see how my presence really helped contribute to the success of the project."
+      ),
       // new Experience(
       //   "assets/img/uom.png",
       //   "School of Computer Science",
